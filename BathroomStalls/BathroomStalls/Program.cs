345aa18e5bf23681056922e0021d3799f88789f4
Giuseppe Mumolo <giuseppemumolo@gmail.com>
2017-04-09 13:42:58 +0200

    idea soluzione

--- BathroomStalls/BathroomStalls/Program.cs
+++ BathroomStalls/BathroomStalls/Program.cs
@@ -10,6 +10,12 @@ namespace BathroomStalls
     {
         static void Main(string[] args)
         {
+            //prendo la partizione più grossa (se ce ne sono più di una ne prendo la prima)
+            //mi metto al centro della partizione (preferendo la sx se il numero di slot è pari) dividendo di fatto la partizione in due parti
+            //lo faccio per tutti i clienti.
+            //alla fine prendo la partizione più grossa mi metto al centro (preferendo la sx) e vedo quanto avrei a sx e qnt a dx? questo è il risultato
+
+            //in questo modo evito di fare mille calcoli per ogni cliente (1 macro calcolo per ogni posto libero)
         }
     }
 }
